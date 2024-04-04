23ffb1d706e552ace2be6e6889488399d5b6fa47
ccbruce <4055588+ccbruce@users.noreply.github.com>
2024-04-04 11:52:17 +0000

    init

---
+++ runtime/vol/kacloud/var/www/html/apps/related_resources/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nextcloud/related_resources',
+        'pretty_version' => 'dev-stable28',
+        'version' => 'dev-stable28',
+        'reference' => '66d2e5d9acdd8e293dcd5275055756740270caa9',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'nextcloud/related_resources' => array(
+            'pretty_version' => 'dev-stable28',
+            'version' => 'dev-stable28',
+            'reference' => '66d2e5d9acdd8e293dcd5275055756740270caa9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
