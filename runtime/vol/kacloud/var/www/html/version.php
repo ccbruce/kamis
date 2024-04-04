7d798cd86e9903429a406afaee4b3da7ce954f88
ccbruce <4055588+ccbruce@users.noreply.github.com>
2024-04-04 14:14:19 +0000

    init

---
+++ runtime/vol/kacloud/var/www/html/version.php
@@ -0,0 +1,19 @@
+<?php 
+$OC_Version = array(28,0,3,2);
+$OC_VersionString = '28.0.3';
+$OC_Edition = '';
+$OC_Channel = 'stable';
+$OC_VersionCanBeUpgradedFrom = array (
+  'nextcloud' => 
+  array (
+    '27.0' => true,
+    '27.1' => true,
+    '28.0' => true,
+  ),
+  'owncloud' => 
+  array (
+    '10.13' => true,
+  ),
+);
+$OC_Build = '2024-02-29T07:49:38+00:00 f7470ab973d9dc3c979accd95985d71cf80653fa';
+$vendor = 'nextcloud';
