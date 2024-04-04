eb4d2d091c497c59ace9ca12855f5c5c75889433
ccbruce <4055588+ccbruce@users.noreply.github.com>
2024-04-04 11:49:41 +0000

    init

---
+++ runtime/vol/kacloud/var/www/html/apps/bruteforcesettings/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nextcloud/bruteforcesttings',
+        'pretty_version' => 'dev-stable28',
+        'version' => 'dev-stable28',
+        'reference' => '0c0476f1ed9b5f280da78eb33661912664628ede',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'nextcloud/bruteforcesttings' => array(
+            'pretty_version' => 'dev-stable28',
+            'version' => 'dev-stable28',
+            'reference' => '0c0476f1ed9b5f280da78eb33661912664628ede',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
---
+++ runtime/vol/kacloud/var/www/html/apps/circles/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nextcloud/circles',
+        'pretty_version' => 'dev-stable28',
+        'version' => 'dev-stable28',
+        'reference' => 'a4bcff058a39337b8b54e3033dd4621d5849a695',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'nextcloud/circles' => array(
+            'pretty_version' => 'dev-stable28',
+            'version' => 'dev-stable28',
+            'reference' => 'a4bcff058a39337b8b54e3033dd4621d5849a695',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
